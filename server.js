5c409387e99d699daf4676cbac512ec21415a497
Ganesh <ganeshpalacharla@outlook.com>
2020-11-10 02:17:10 -0500

    changed final code

--- server.js
+++ server.js
@@ -9,18 +9,19 @@ connectDB();
 //Init middleware
 app.use(express.json({extended:false}));
 
-//serve static assets in production
-if(process.env.NODE_ENV === 'production'){
-  //set static folder
-  app.use(express.static('client/build'));
-  app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')));
-}
 
 //define routes
 
 app.use('/api/users',require('./routes/users'));
 app.use('/api/contacts',require('./routes/Contacts'));
 app.use('/api/auth',require('./routes/auth'));
+
+//serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+  //set static folder
+  app.use(express.static('client/build'));
+  app.get('*',(req,res)=>res.sendFile(path.resolve(__dirname,'client','build','index.html')));
+}
 
 const PORT=process.env.PORT || 5000;
 
